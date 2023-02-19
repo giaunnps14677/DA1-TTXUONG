61b9c7310625fe4cf36786a907098d42d3bbbe53
MinhxThanh <lethanh.000113@gmail.com>
2023-02-19 14:53:11 +0700

    Initial commit

---
+++ src/main/java/com/poly/dax/Da1TtxuongApplication.java
@@ -0,0 +1,13 @@
+package com.poly.dax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Da1TtxuongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Da1TtxuongApplication.class, args);
+    }
+
+}
